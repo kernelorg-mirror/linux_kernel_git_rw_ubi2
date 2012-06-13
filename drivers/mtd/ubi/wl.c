e110018d31cda0a0855279e85caa8043868a81cb
Richard Weinberger <richard@nod.at>
2012-06-13 12:42:00 +0200

    UBI: Fastmap: Remove useless code
    This case can not happen. No need to check for it.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- drivers/mtd/ubi/wl.c
+++ drivers/mtd/ubi/wl.c
@@ -1664,8 +1664,6 @@ int ubi_wl_init(struct ubi_device *ubi, struct ubi_attach_info *ai)
 			e->pnum = aeb->pnum;
 			e->ec = aeb->ec;
 			ubi->lookuptbl[e->pnum] = e;
-			if (ubi_is_fm_block(ubi, aeb->pnum))
-				continue;
 
 			if (!aeb->scrub) {
 				dbg_wl("add PEB %d EC %d to the used tree",
