4babd6302223be16de6f8d1d06d72f4f36c3499d
Richard Weinberger <richard@nod.at>
2012-06-15 09:59:12 +0200

    Revert "UBI: Fastmap: Check for duplicated PEBs in add_aeb()"
    This reverts commit 95bbb0d1b2ea86103edfdd4b07ddfb058b1e0eff.
    
    That check is no longer needed.
    It must not happen that fastmap creates duplicates.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- drivers/mtd/ubi/fastmap.c
+++ drivers/mtd/ubi/fastmap.c
@@ -54,10 +54,6 @@ static int add_aeb(struct ubi_attach_info *ai, struct list_head *list,
 {
 	struct ubi_ainf_peb *aeb;
 
-	list_for_each_entry(aeb, list, u.list)
-		if (aeb->pnum == pnum)
-			return 0;
-
 	aeb = kmem_cache_alloc(ai->aeb_slab_cache, GFP_KERNEL);
 	if (!aeb)
 		return -ENOMEM;
